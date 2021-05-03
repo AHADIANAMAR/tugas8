9d44a5bc102ec5272648850678eea1881dd62cf5
AHADIANAMAR <79738225+AHADIANAMAR@users.noreply.github.com>
2021-05-03 10:47:38 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+// konfigurasi databas
+$host="localhost";
+$user="root";
+$password="";
+$database="db_daftarpeserta";
+// perintah php untuk akses ke database
+$koneksi=mysqli_connect($host, $user, $password, $database);
+?>
